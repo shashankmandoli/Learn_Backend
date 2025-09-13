bbaf52c14b9068a645c5bb76876c5e0b62c7830f
shashank mandoli <shashankmandoli2004@gmail.com>
2025-09-13 21:55:01 +0530

    Add Custom Api Response and Error Handling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
-
+import app from "./app.js"
 import mongoose from "mongoose"
 import connectDB from "./db/index.js";
 
 dotenv.config()
 
-connectDB();
+connectDB()
+.then( () => {
+    app.on("error", (error) => {
+        console.log("ERR: ",error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB Connection Fail !!! ", err);
+    
+})
 
 
 
