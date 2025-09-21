624d199dc713bc02999825b9868882351de9a412
shashank mandoli <shashankmandoli2004@gmail.com>
2025-09-21 12:04:39 +0530

    Add User Controllers and Routes

--- src/app.js
+++ src/app.js
@@ -14,4 +14,12 @@ app.use(express.urlencoded({extended: true, limit: '16kb'}))  // Handling of dif
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+//routes
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+
 export default app
